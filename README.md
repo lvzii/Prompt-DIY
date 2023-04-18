c973bc56535c49c171f3b00e20dc13c64afb955b
lvzi <zjs20001205@gmail.com>
2023-04-18 10:51:38 +0800

    feat: auto generate interface

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Prompt-DIY
-build prompt-input-output pairs for yourself
+
+该项目旨在训练自己的指令模型。
+
+<font size=3>build prompt-input-output pairs for yourself</font> 
+
+目前的功能是提供网页入口，将自己平时整理的指令和答案进行存储。
+
+该项目也可以优化自己的工作流程，在input输入自己的任务，点击生成按钮调用大模型给出参考，最终修改后点击提交写入数据库。
+
+生成按钮调用的功能需要自行配置。
+
 
 # Startup
 
 修改python解释器路径，双击`start.bat`
+
+# TODO
+
+加入调用接口，生成预编写的内容
--- app.py
+++ app.py
@@ -34,18 +34,34 @@ def write_to_db(prompt: str, input: str, output: str):
     return "数据已成功写入数据库！"
 
 
+# 依据prompt和input的输入，调用接口生成output
+def write_to_output(prompt: str, input: str):
+    # 接口需要自己设定
+    from generate_api import chatgpt
+    return chatgpt(prompt + input)
+
+
+def func_clear(prompt, input, output, flag):
+    return "", "", "", ""
+
+
+# examples = [[i, "", ""] for i in candidate_prompts],
+
 # 定义Gradio界面
-iface = gr.Interface(
-    title=title,
-    examples=[[i, "", ""] for i in candidate_prompts],
-    fn=write_to_db,
-    inputs=[
-        gr.inputs.Textbox(label="prompt"),
-        gr.inputs.Textbox(label="input"),
-        gr.inputs.Textbox(label="output")
-    ],
-    outputs=gr.outputs.Textbox(label="写入情况")
-)
+with gr.Blocks() as iface:
+    gr.Markdown("Build yourself prompt")
+    with gr.Tab("Build"):
+        text_prompt = gr.inputs.Textbox(label="prompt")
+        text_input = gr.inputs.Textbox(label="input")
+        text_output = gr.inputs.Textbox(label="output")
+        text_flag = gr.outputs.Textbox(label="写入情况")
+        btn_generate = gr.Button("生成")
+        btn_submit = gr.Button("提交")
+        btn_clear = gr.Button("清空")
+        btn_generate.click(write_to_output, inputs=[text_prompt, text_input], outputs=text_output)
+        btn_submit.click(write_to_db, inputs=[text_prompt, text_input, text_output], outputs=text_flag)
+        btn_clear.click(func_clear, inputs=[text_prompt, text_input, text_output, text_flag],
+                        outputs=[text_prompt, text_input, text_output, text_flag])
 
 # 启动Gradio界面
 iface.launch()
