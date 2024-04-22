9c9cd806dfb08049ae55f2d0cddc149143ae4635
lvzi <zjs20001205@gmail.com>
2024-04-22 10:53:27 +0800

    perf: rename var

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ TABLE_NAME = "instruct"
 title = "Prompt-DIY"
 
 candidate_prompts = [
+    "按照下面样例格式生成NER结果",
     "润色下面这段文本",
     "对指定内容进行多个版本的改写，以避免文本重复。",
     "改写这篇文章",
@@ -21,7 +22,8 @@ candidate_prompts = [
     "根据以下大纲，扩写该章节",
     "给这个故事添加一个反转的剧情",
     "给这个故事改成一个有反转的剧情",
-    "写一个小说开头"
+    "写一个小说开头",
+    "按照下面的需求写代码",
     "其他",
     ""
 ]
@@ -30,13 +32,17 @@ engine = create_engine(f'sqlite:///{DB_NAME}.db?check_same_thread=False', echo=T
 
 
 # 定义函数，将文本框中的内容写入数据库
-def write_to_db(prompt: str, input: str, output: str):
+def write_to_db(prompt: str, input: str, output: str, reference: str):
+    if output == reference:
+        reference = ""
+
     df = pd.DataFrame(
         {
             "insert_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "prompt": prompt,
             "input": input,
-            "output": output
+            "output": output,
+            "rejected": reference
         },
         index=[str(uuid.uuid4())]
     )
@@ -55,6 +61,10 @@ def write_to_output(prompt: str, input: str, method):
         elif method == "chatglm":
             from generate_api import chatglm
             return chatglm(prompt + "\n" + input)
+        elif method == "moss":
+            from generate_api import chatmoss
+            return chatmoss(prompt + "\n" + input)
+
         else:
             return ""
     except:
@@ -76,7 +86,7 @@ with gr.Blocks() as iface:
                                inputs=[text_prompt])
 
         text_input = gr.inputs.Textbox(label="input")
-        x = gr.Radio(["chatglm", "chatgpt", "none"])
+        x = gr.Radio(["chatglm", "chatgpt", "moss", "none"])
         btn_generate = gr.Button("生成")
         text_reference = gr.inputs.Textbox(label="reference")
         btn_generate.click(write_to_output, inputs=[text_prompt, text_input, x], outputs=text_reference)
@@ -86,7 +96,7 @@ with gr.Blocks() as iface:
         text_flag = gr.outputs.Textbox(label="写入情况")
         btn_clear = gr.Button("清空")
         btn_transfer.click(lambda x: x, inputs=[text_reference], outputs=[text_output])
-        btn_submit.click(write_to_db, inputs=[text_prompt, text_input, text_output], outputs=text_flag)
+        btn_submit.click(write_to_db, inputs=[text_prompt, text_input, text_output, text_reference], outputs=text_flag)
         btn_clear.click(func_clear, inputs=[text_prompt, text_input, text_reference, text_output, text_flag],
                         outputs=[text_prompt, text_input, text_output, text_flag])
         text_clipboard = gr.Textbox(label="clipboard")
